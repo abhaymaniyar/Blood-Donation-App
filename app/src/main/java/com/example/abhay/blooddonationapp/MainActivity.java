07fbc25707154ae07d86a13b7351575e5aa96f34
abhaymaniyar <abhaymaniyarhack@gmail.com>
2017-07-20 15:02:31 +0530

    Main activity converted into fragment successfully

--- app/src/main/java/com/example/abhay/blooddonationapp/MainActivity.java
+++ app/src/main/java/com/example/abhay/blooddonationapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,15 +14,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -33,6 +26,10 @@ public class MainActivity extends AppCompatActivity
         SharedPreferences sharedPreferences = this.getSharedPreferences("registrationStatus", Context.MODE_PRIVATE);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Fragment fragment = new MainFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().add(R.id.fragment_frame, fragment).commit();
 
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -49,54 +46,6 @@ public class MainActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-        final Spinner spinner = (Spinner) findViewById(R.id.search_spinner);
-        final List<String> bloodGroups = new ArrayList<String>();
-        bloodGroups.add("A+");
-        bloodGroups.add("A");
-        bloodGroups.add("B+");
-        bloodGroups.add("O+");
-        bloodGroups.add("B-");
-        bloodGroups.add("AB-");
-        bloodGroups.add("O-");
-        bloodGroups.add("B");
-        //        set Adapter to Spinner items
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, bloodGroups);
-        dataAdapter.setDropDownViewResource(R.layout.spinner_item);
-        spinner.setAdapter(dataAdapter);
-        spinner.setPrompt(bloodGroups.get(0));
-//        select a bloodGroup and show it on spinner
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                String item = adapterView.getItemAtPosition(i).toString();
-                spinner.setPrompt(item);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-
-//        onClick behavior of the search button
-        Button searchDonorBtn = (Button) findViewById(R.id.search_button);
-        searchDonorBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EditText cityEditText = (EditText) findViewById(R.id.search_city);
-                String city = cityEditText.getText().toString();
-                Spinner spnr = (Spinner) findViewById(R.id.search_spinner);
-                String bGroup = spnr.getPrompt().toString();
-                Intent resultsActivity = new Intent(MainActivity.this, ResultsActivity.class);
-                resultsActivity.putExtra("city", city);
-                resultsActivity.putExtra("bGroup", bGroup);
-                startActivity(resultsActivity);
-            }
-        });
     }
 
     @Override
---
+++ app/src/main/java/com/example/abhay/blooddonationapp/MainFragment.java
@@ -0,0 +1,122 @@
+package com.example.abhay.blooddonationapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by abhay on 20/7/17.
+ */
+
+public class MainFragment extends Fragment{
+    public MainFragment() {
+        super();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.main_fragment, container, false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SharedPreferences sharedPreferences = getContext().getSharedPreferences("registrationStatus", Context.MODE_PRIVATE);
+//        Toolbar toolbar = (Toolbar) getView().findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+//        NavigationView navigationView = (NavigationView) getView().findViewById(R.id.nav_view);
+//        navigationView.setNavigationItemSelectedListener(this);
+
+        final Spinner spinner = (Spinner) getView().findViewById(R.id.search_spinner);
+        final List<String> bloodGroups = new ArrayList<String>();
+        bloodGroups.add("A+");
+        bloodGroups.add("A");
+        bloodGroups.add("B+");
+        bloodGroups.add("O+");
+        bloodGroups.add("B-");
+        bloodGroups.add("AB-");
+        bloodGroups.add("O-");
+        bloodGroups.add("B");
+        //        set Adapter to Spinner items
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, bloodGroups);
+        dataAdapter.setDropDownViewResource(R.layout.spinner_item);
+        spinner.setAdapter(dataAdapter);
+        spinner.setPrompt(bloodGroups.get(0));
+//        select a bloodGroup and show it on spinner
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String item = adapterView.getItemAtPosition(i).toString();
+                spinner.setPrompt(item);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+//        onClick behavior of the search button
+        Button searchDonorBtn = (Button) getView().findViewById(R.id.search_button);
+        searchDonorBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText cityEditText = (EditText) getView().findViewById(R.id.search_city);
+                String city = cityEditText.getText().toString();
+                Spinner spnr = (Spinner) getView().findViewById(R.id.search_spinner);
+                String bGroup = spnr.getPrompt().toString();
+                Intent resultsActivity = new Intent(getContext(), ResultsActivity.class);
+                resultsActivity.putExtra("city", city);
+                resultsActivity.putExtra("bGroup", bGroup);
+                startActivity(resultsActivity);
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
