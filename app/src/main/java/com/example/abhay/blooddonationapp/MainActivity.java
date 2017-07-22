ed8e7c37522d8efbf8630e5ecefc71cf99918051
abhaymaniyar <abhaymaniyarhack@gmail.com>
2017-07-22 13:58:22 +0530

    Fix sidebar bg color and icon

--- app/src/main/java/com/example/abhay/blooddonationapp/MainActivity.java
+++ app/src/main/java/com/example/abhay/blooddonationapp/MainActivity.java
@@ -12,7 +12,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -69,12 +68,12 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
 
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -85,7 +84,7 @@ public class MainActivity extends AppCompatActivity
         String TAG = ">>>>";
         if (id == R.id.nav_search) {
             fragmentOther = new MainFragment();
-            setTitle("Search Donor");
+            setTitle("Search Donors");
         } else if (id == R.id.nav_register) {
             SharedPreferences sharedPreferences = getSharedPreferences("registrationStatus", Context.MODE_PRIVATE);
             boolean isRegistered = sharedPreferences.getBoolean("isRegistered", false);
