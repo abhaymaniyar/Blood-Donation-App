28953c69e5eee5f3b153abb233df2e0b67bdede6
abhaymaniyar <abhaymaniyarhack@gmail.com>
2017-08-09 18:35:06 +0530

    Add mail icon and edit about us section

--- app/src/main/java/com/example/abhay/blooddonationapp/AboutUsFragment.java
+++ app/src/main/java/com/example/abhay/blooddonationapp/AboutUsFragment.java
@@ -8,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -37,12 +38,13 @@ public class AboutUsFragment extends Fragment {
 
         TextView details = (TextView) getView().findViewById(R.id.details);
         details.setText("Hello there! \n" +
-                "A little about myself, I am Abhay Maniyar and I am an Computer Science Engineer and like to solve problems using the power of technology.\n" +
-                "This app is one of my ways to give back to the society and I hope you will help me with it by keeping this app installed on your phone. \n" +
-                "Keeping this app installed will allow you to help even some random guy by a simple search on this app.\n" +
+                "A little about myself, I am Abhay Maniyar and I am a Computer Science Engineer and like to solve problems using the power of technology.\n" +
+                "This app is one of my ways to solve a problem and give back to the society and I hope you will help me with it by keeping this app installed on your phone. \n" +
+                "Keeping this app installed will allow you to help even some random guy by a simple search for potential donors on this app.\n" +
                 "And I am not integrating advertisments on this app so you don't get any useless notifications on your phone.\n" +
                 "This app is like a dialer, you only use it when you or someone you know needs it.\n" +
-                "I have open sourced this app, so if you want to improve this app or find any bug feel free to raise an issue on the GitHub repo linked below.");
+                "I have open sourced this app, so if you want to improve this app or find any bug feel free to raise an issue on the GitHub repo linked below.\n"+
+                "You can also contact me on my email by clicking below icon.");
 
         LinearLayout githubIcon = (LinearLayout) getView().findViewById(R.id.github_icon);
         githubIcon.setOnClickListener(new View.OnClickListener() {
@@ -50,6 +52,18 @@ public class AboutUsFragment extends Fragment {
             public void onClick(View view) {
                 Intent i = new Intent(Intent.ACTION_VIEW);
                 i.setData(Uri.parse("http://www.github.com/abhaymaniyar/Blood-Donation-App"));
+                startActivity(i);
+            }
+        });
+
+        ImageView email = (ImageView) githubIcon.findViewById(R.id.email_icon);
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse("mailto:abhaymaniyar@live.in"));
+//                i.setType("plain/text");
+//                i.putExtra(Intent.EXTRA_EMAIL, "");
                 startActivity(i);
             }
         });
